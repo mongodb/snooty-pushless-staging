49b58c253f58710cd16e618a5d2a94ab4773488d
madelinezec <mez2113@columbia.edu>
2020-02-18 15:34:21 -0500

    test

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ async function main() {
 
   try {
     repoInfo = await StagingUtils.getRepoInfo();
-    console.log(repoInfo);
+    console.log("this is repo info ", repoInfo);
     user = StagingUtils.getGitUser(repoInfo);
     console.log(repoInfo, user);
   } catch (error) {
@@ -80,7 +80,7 @@ async function main() {
   }
 
   const [repoOwner, repoName] = upstreamOwnerAndName.split('/');
-  console.log(repoOwner, repoName, upstreamOwnerAndName);
+  console.log(33333, repoOwner, repoName, upstreamOwnerAndName);
   const branchName = upstreamConfig.split('/')[1];
   const url = `https://github.com/${user}/${repoName}.git`;
   console.log("this is the url ", url, `https://github.com/${user}/${repoName}.git`);
