d4c89f825eb9f296cb5e9d012727c3bb3f15aa31
madelinezec <mez2113@columbia.edu>
2020-02-18 13:49:39 -0500

    Update stagingUtils.js

--- stagingUtils.js
+++ stagingUtils.js
@@ -89,7 +89,7 @@ module.exports = {
       localBranchName: localBranchArg,
       isFork: true,
       private: false,
-      isXlarge: false,
+      isXlarge: true,
       repoOwner: repoOwnerArg,
       url: urlArg,
       newHead: lastCommit,
